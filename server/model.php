75dbc6aa655814b119ee227f63b76eecfc13be86
Gautier Charline <gautier58@mmi-dev.unilim.fr>
2026-04-22 13:51:39 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "gautier58");
+define("DBLOGIN", "gautier58");
+define("DBPWD", "gautier58");
 
 
 function getAllMovies(){

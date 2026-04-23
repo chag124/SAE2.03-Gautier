df8d2424334b5e0d437378828d9297d1b458d023
Gautier Charline <gautier58@mmi-dev.unilim.fr>
2026-04-23 14:57:00 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -57,6 +57,6 @@ function insertMovie($name, $director, $year, $time, $description, $category, $i
         ]);
         return true;
     } catch (PDOException $e){
-        return false;
+        die($e->getMessage());;
     }
 }

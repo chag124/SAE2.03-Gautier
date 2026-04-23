28da2d0a5e9ccb1e991cab006d84b43b18a2b7e1
Gautier Charline <gautier58@mmi-dev.unilim.fr>
2026-04-23 16:06:24 +0200

    update credentials

---
+++ app/component/MovieDetail/script.js
@@ -0,0 +1,29 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
+
+let MessagetemplateFile = await fetch("./component/Movie/message.html");
+let Messagetemplate = await MessagetemplateFile.text();
+
+let Movie = {};
+
+Movie.format = function (data) {
+  //data = [];
+
+  if (!data || data.length == 0) {
+    return Messagetemplate;
+  }
+
+  let htmlfinal = "";
+
+  for (let movie of data) {
+    let html = template;
+
+    html = html.replace("{{name}}", movie.name);
+    html = html.replace("{{image}}", movie.image);
+    htmlfinal += html;
+  }
+  return htmlfinal;
+};
+
+export { Movie };

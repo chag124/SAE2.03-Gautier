755063b5bf7219a5e2df39b5877791eb3e5733db
Gautier Charline <gautier58@mmi-dev.unilim.fr>
2026-04-24 08:45:00 +0200

    update credentials

--- app/data/dataMovie.js
+++ app/data/dataMovie.js
@@ -16,7 +16,7 @@ DataMovie.requestMovies = async function () {
     return data;
 }
 
-DataMovie.requestMovieDetail = async function (id) {
+DataMovie.requestMovieDetails = async function (id) {
     let answer = await fetch(HOST_URL + "/server/script.php?todo=readMovieDetail&id=" + id);
     let data = await answer.json();
     return data;
